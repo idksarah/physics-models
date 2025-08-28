bfe1c80b56e2b3378d2c12d9fe9046b2bbae2a0e
idksarah <sarah.p.ngai@gmail.com>
2025-08-28 00:41:31 -0700

    initial commit; make window

---
+++ pendulum/pendulum/Main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+	// initialize glfw
+	glfwInit();
+
+	// set glfw major minor versions
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Pendulum Simulation", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	
+	glfwTerminate();
+	return 0;
+}
