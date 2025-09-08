81eb8659180d3b6270e44e6df624fca08e091c9e
idksarah <sarah.p.ngai@gmail.com>
2025-09-08 01:06:51 -0700

    circle!!

--- pendulum/pendulum/Main.cpp
+++ pendulum/pendulum/Main.cpp
@@ -1,116 +1,90 @@
 #include <iostream>
-#include <glad/glad.h>
+//#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <Gl/gl.h>
+#include <math.h>
+#include <windows.h>
+#include <vector>
 
 
-// Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\n\0";
+void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main() {
+	double SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
 	// initialize glfw
 	glfwInit();
 
 	// set glfw major minor versions
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// define triangle vertices
-	GLfloat vertices[] = {
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
-	};
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
 	// create window
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Pendulum Simulation", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pendulum Simulation", NULL, NULL);
 
 	glfwMakeContextCurrent(window);
 
-	gladLoadGL();
+	//gladLoadGL();
 
 	// set screen size
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	// create vertex shader
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// create fragment shader
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	GLuint shaderProgram = glCreateProgram();
-
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	GLuint VAO, VBO;
-
-	// generate vao (vertex array object; stores pointers to vbos) and vbo (vertex buffer object)
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	// set vao as current array
-	glBindVertexArray(VAO);
-
-	// set vbo as current buffer
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// configure vbo
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	// unbind buffer and vertex array
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	// specify bg color
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
-
-	// main while loop
+	// main loop
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClearColor(.2f, .3f, .3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glfwSwapBuffers(window);
 
+		drawCircle(0, 0, 0, .5, 36);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	// clean program
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
-
-	glfwDestroyWindow(window);
-	
 	// quit
 	glfwTerminate();
 	return 0;
+}
+
+void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides) {
+	GLint numberOfVertices = numberOfSides + 2;
+	GLfloat doublePi = 2.0f * M_PI;
+
+	std::vector<GLfloat> circleVerticesX(numberOfVertices);
+	std::vector<GLfloat> circleVerticesY(numberOfVertices);
+	std::vector<GLfloat> circleVerticesZ(numberOfVertices);
+
+	circleVerticesX[0] = x;
+	circleVerticesY[0] = y;
+	circleVerticesZ[0] = z;
+
+	for (int i = 1; i < numberOfVertices; i++) {
+		circleVerticesX[i] = x + cos(i * doublePi / numberOfSides);
+		circleVerticesY[i] = y + sin(i * doublePi / numberOfSides);
+		circleVerticesZ[i] = z;
+	}
+
+	std::vector<GLfloat> allCircleVertices(numberOfVertices * 3);
+
+	for (int i = 0; i < numberOfVertices; i++) {
+		allCircleVertices[i * 3] = circleVerticesX[i];
+		allCircleVertices[i * 3 + 1] = circleVerticesY[i];
+		allCircleVertices[i * 3 + 2] = circleVerticesZ[i];
+	}
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, allCircleVertices.data()); 
+	glDrawArrays(GL_TRIANGLE_FAN, 0, numberOfVertices);
+	glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
